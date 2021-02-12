80765eb0414a37e8c54b4656f89f3a3daed9a3bf
Simon-Oliver <stauffer.simon@outlook.com>
2021-02-12 22:31:14 +0100

    serialport working

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,108 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import icon from '../assets/icon.svg';
 import moment from 'moment';
 import fs from 'fs';
 
 const { remote } = require('electron');
+const serialPort = remote.require('serialport');
+// const list = remote.require('@serialport/list');
+const bindings = remote.require('@serialport/bindings');
+
+var listOfPorts = [];
 
 // Importing dialog module using remote
 const dialog = remote.dialog;
 
 import './App.global.css';
 
+const Readline = serialPort.parsers.Readline;
+
 const Hello = () => {
   const [state, setState] = useState({ data: 'test' });
+  const [closed, setClosed] = useState(true);
+
+  const connect = async () => {
+    const list = await serialPort.list();
+
+    list.forEach((e) => {
+      if (e.manufacturer === 'SparkFun') {
+        var sp = new serialPort(e.path, {
+          baudRate: 9600,
+        });
+        sp.open(() => {
+          setClosed(false);
+          console.log('Opened');
+        });
+        const parser = sp.pipe(new Readline({ delimiter: '\r\n' }));
+        parser.on('data', function (data) {
+          console.log('Data:', data);
+        });
+
+        sp.on('close', () => {
+          setClosed(true);
+          console.log('Closed');
+        });
+
+        console.log(e.path);
+      }
+    });
+  };
+
+  function useInterval(callback, delay) {
+    const savedCallback = useRef();
+
+    // Remember the latest callback.
+    useEffect(() => {
+      savedCallback.current = callback;
+    }, [callback]);
+
+    // Set up the interval.
+    useEffect(() => {
+      function tick() {
+        savedCallback.current();
+      }
+      if (delay !== null) {
+        let id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    }, [delay]);
+  }
+
+  if (closed) {
+    useInterval(function () {
+      console.log('RECONNECTING TO ARDUINO');
+      connect();
+    }, 1000);
+  }
+
+  useEffect(() => {
+    // //called automatically by bindings.list()
+    // function list(ports) {
+    //   listOfPorts = ports;
+    //   // now listOfPorts will be the port Objects
+    //   listOfPorts.forEach((e) => {
+    //     if (e.manufacturer === 'SparkFun') {
+    //       console.log(e.path);
+    //       var sp = new serialPort(e.path, {
+    //         baudRate: 9600,
+    //       });
+    //       //parse incoming data line-by-line from serial port.
+    //       const parser = sp.pipe(new Readline({ delimiter: '\r\n' }));
+    //       parser.on('open', () => console.log('====== Open ======'));
+    //       parser.on('data', function (data) {
+    //         console.log('Data:', data);
+    //       });
+    //       parser.on('close', function () {
+    //         console.log('---- Disconnected ------');
+    //       });
+    //     }
+    //   });
+    // }
+    // bindings.list().then(list, (err) => {
+    //   process.exit(1);
+    // });
+  }, [closed]);
 
   const changeHandler = (e) => {
     e.preventDefault();
