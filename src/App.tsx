8a5666875bbd8f92ab146063cb79420f5d7980ae
Simon-Oliver <stauffer.simon@outlook.com>
2021-02-14 00:21:57 +0100

    Listing Devices and implemented connect and disconnect

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ import fs from 'fs';
 const { remote } = require('electron');
 const serialPort = remote.require('serialport');
 // const list = remote.require('@serialport/list');
-const bindings = remote.require('@serialport/bindings');
-
-var listOfPorts = [];
+//const bindings = remote.require('@serialport/bindings');
 
 // Importing dialog module using remote
 const dialog = remote.dialog;
@@ -20,51 +18,69 @@ const Readline = serialPort.parsers.Readline;
 
 const Hello = () => {
   const [state, setState] = useState({ data: 'test' });
-  const [closed, setClosed] = useState(true);
+  const [searching, setSearching] = useState(true);
+  const [devices, setDevices] = useState([]);
+  const [openPorts, setOpenPorts] = useState([]);
 
   const intervalId = useRef();
 
   useEffect(() => {
     intervalId.current = setInterval(() => {
-      if (closed) {
+      if (searching) {
         console.log('RECONNECTING TO ARDUINO');
-        connect();
+        getDevices();
       }
     }, 1000);
     return () => clearInterval(intervalId.current);
-  }, [closed]);
+  }, [searching]);
 
-  const connect = async () => {
+  const getDevices = async () => {
     const list = await serialPort.list();
+    const newList = list.filter((e) => e.manufacturer != undefined);
+    setDevices(newList);
+  };
 
-    list.forEach((e) => {
-      if (e.manufacturer === 'SparkFun') {
-        var sp = new serialPort(e.path, {
-          baudRate: 9600,
-        });
-        sp.open(() => {
-          setClosed(false);
-          console.log('Opened');
-        });
-        const parser = sp.pipe(new Readline({ delimiter: '\r\n' }));
-        parser.on('data', function (data) {
-          console.log('Data:', data);
-        });
+  const connectDevice = (path) => {
+    const item = openPorts.filter((e) => e.port.path === path);
+    if (item.length === 0) {
+      var sp = new serialPort(path, {
+        baudRate: 9600,
+      });
 
-        sp.on('close', () => {
-          sp.close(() => console.log('Closed'));
-          setClosed(true);
-        });
+      setOpenPorts([...openPorts, { port: sp, connected: true }]);
+      sp.open(() => {
+        //setClosed(false);
+        console.log('Opened');
+      });
+      const parser = sp.pipe(new Readline({ delimiter: '\r\n' }));
+      parser.on('data', function (data) {
+        setState({ data });
+        console.log('Data:', data);
+      });
 
-        console.log(e.path);
-      }
-    });
+      sp.on('close', () => {
+        sp.close(() => console.log('Closed'));
+        const items = openPorts.filter((e) => e.port.path != path);
+        setOpenPorts([...items]);
+      });
+    } else {
+      item[0].port.close(() => console.log('Closed'));
+      const items = openPorts.filter((e) => e.port.path != path);
+      setOpenPorts([...items]);
+      console.log(`Device at ${path} is alread connected`);
+    }
   };
 
   const changeHandler = (e) => {
     e.preventDefault();
     const data = e.target.value;
     setState({ data });
+  };
+
+  const onClickHandler = (e) => {
+    // e.stopPropagation();
+    console.log(e.target.dataset.path);
+    connectDevice(e.target.dataset.path);
   };
 
   const submitHandler = (e) => {
@@ -92,13 +108,26 @@ const Hello = () => {
       });
   };
 
+  const renderList = (el) => {
+    const paths = openPorts.map((e) => e.port.path);
+
+    return el.map((e, i) => (
+      <div
+        onClick={(e) => onClickHandler(e)}
+        data-index={i}
+        data-path={e.path}
+        key={e.path}
+      >
+        {e.manufacturer}
+        <span onClick={(e) => e.stopPropagation()}>
+          {paths.indexOf(e.path) !== -1 ? 'Connected' : 'Not Connected'}
+        </span>
+      </div>
+    ));
+  };
+
   return (
     <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <span>{moment().format()}</span>
       <form
         onChange={(e) => changeHandler(e)}
         onSubmit={(e) => submitHandler(e)}
@@ -110,6 +139,7 @@ const Hello = () => {
         <input type="submit" value="Submit" />
       </form>
       <div>{state.data}</div>
+      <div>{renderList(devices)}</div>
     </div>
   );
 };
